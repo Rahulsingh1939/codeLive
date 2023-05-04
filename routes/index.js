110bc7ba4d8c70759c92f1f600cf1a010f066afb
Rahulsingh1939 <rahulsingh19390@gmail.com>
2023-05-05 00:01:30 +0530

    About page work

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express JS' });
+  res.render('index', { title: 'CodeLive - a Collaborative Coding platform.' });
 });
 
+//About Page
+router.get('/about', function(req,res,next){
+  res.render('about',{ title:'CodeLive - a Collaborative Coding platform.'})
+})
 module.exports = router;

33da10cf78e493b7a06ab0bc09bc825409e74192
Rahulsingh1939 <rahulsingh19390@gmail.com>
2023-05-05 12:12:04 +0530

    Working on Validation

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(exressValidator())
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 

4baf4f842a6870e79495573a9c9465d48e9413d9
Rahulsingh1939 <rahulsingh19390@gmail.com>
2023-05-05 12:16:00 +0530

    Working on Validation

--- routes/index.js
+++ routes/index.js
@@ -20,6 +20,9 @@ router.route('/contact')
     req.checkBody('name','Invalid Name').notEmpty();
     req.checkBody('email','Invalid Email').isEmail();
     req.checkBody('message','Empty Mesaage').notEmpty();
+    var errors=req.validationErrors();
+    
+
     res.render('thank',{ title : 'CodeLive - a Collaborative Coding platform.'})
   });
   

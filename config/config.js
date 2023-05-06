ed79df05a2baf781722e4920503f9c7e4f5a340a
Rahulsingh1939 <rahulsingh19390@gmail.com>
2023-05-06 09:21:31 +0530

    Setting up Login and Register

---
+++ config/config.js
@@ -0,0 +1,11 @@
+'use strict'
+
+module.export = {
+    mailer:{
+        service: 'Gmail',
+        auth : {
+            user: 'demoBinaryRahul@gmail.com',
+            pass: 'demoBinaryRahul@gmail'
+        }
+    }
+};

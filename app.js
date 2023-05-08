6b226f659fa3d087366db30702123c4a6c97d7c1
Rahulsingh1939 <rahulsingh19390@gmail.com>
2023-05-08 12:46:11 +0530

    Working on Login And Register validATION

--- app.js
+++ app.js
@@ -6,10 +6,14 @@ var logger = require('morgan');
 var expressValidator = require('express-validator');
 
 var mongoose =require('mongoose');
+var passport = require('passport');
+var session= require('express-session');
+require('./passport/passport.js')
+
 var config = require('./config/config.js');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var authRouter = require('./routes/auth');
 
 mongoose.connect('mongodb://127.0.0.1:27017/codelive');
 global.User= require('./models/user.js');
@@ -25,10 +29,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(cookieParser());
+app.use(session({
+  secret: config.skey,
+  resave:false,
+  saveUninitialized:true
+}))
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/auth', authRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/auth.js
+++ routes/auth.js
@@ -12,6 +12,30 @@ router.get('/login', function(req,res,next){
   .get( function(req,res,next){
     res.render('register',{ title : 'Register - CodeLive'})
   })
-  .post(function(req,res,next){
-    
-  });
+  .post(async function(req,res,next){
+    await body('name').notEmpty().withMessage('Invalid Name').run(req),
+    await body('email').isEmail().withMessage('Invalid Email').run(req),
+    await body('password').notEmpty().withMessage('Empty Password').run(req),
+    await body('password').notEmpty().equals(req.body.confirmPassword).withMessage('Password Do not match').run(req)
+    var errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.render('register', {
+            title: 'Register - CodeLive',
+            name: req.body.name,
+            email: req.body.email,
+            errorMessages: errors.array()
+        });
+        }else{
+            var user = new User();
+            user.name = req.body.name;
+            user.email = req.body.email;
+            user.setPassword(req.body.password);
+            user.save(function(err){
+                if(err){
+                    res.render('register', { title: 'Register - CodeLive', errorMessages: err });
+                } else{
+                    res.redirect('/login');
+                }
+            });
+        }
+        });

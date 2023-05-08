3e50e132ee49ee5032722770ce50fad23603072f
Rahulsingh1939 <rahulsingh19390@gmail.com>
2023-05-08 09:30:43 +0530

    Working on Db

--- config/config.js
+++ config/config.js
@@ -7,5 +7,6 @@ module.export = {
             user: 'demoBinaryRahul@gmail.com',
             pass: 'demoBinaryRahul@gmail'
         }
-    }
+    },
+    dbConnString:'mongodb://127.0.0.1:27017/codelive'
 };

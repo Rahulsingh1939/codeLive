08cfd6212d784d0167728e1f4d2263b9efa40c6f
Rahulsingh1939 <rahulsingh19390@gmail.com>
2023-05-09 17:17:23 +0530

    Added FaceBook authentication

--- passport/passport.js
+++ passport/passport.js
@@ -1,5 +1,6 @@
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
+var FacebookStrategy=require('passport-facebook').Strategy;
 
 passport.serializeUser(function (user, done) {
   done(null, user._id);
@@ -34,3 +35,40 @@ passport.use(new LocalStrategy({
     }
   }));
 
+
+  passport.use(new FacebookStrategy({
+    clientID:'755027186361839',
+    clientSecret:'e70be3e90a1bcec691af99df2044e228',
+    callBackURL:'http://localhost:3000/auth/facebook/callback'
+  },
+  function(token, refreshToken, profile, done) {
+    User.findOne({'facebookId': profile.id}, function(err, user) {
+      if (err) return done(err);
+
+      if (user) {
+        return done(null, user);
+      } else {
+        User.findOne({email: profile.emails[0].value}, function (err, user) {
+          if (user) {
+            user.facebookId = profile.id
+            return user.save(function (err) {
+              if (err) return done(null, false, { message: "Can't save user info"});
+              return done(null, user);
+            })
+          }
+
+          var user = new User();
+          user.name = profile.displayName;
+          user.email = profile.emails[0].value;
+          user.facebookId = profile.id
+          user.save(function (err) {
+            if (err) return done(null, false, { message: "Can't save user info"});
+            return done(null, user);
+          });
+        })
+      }
+
+
+    });
+  }
+));
--- routes/auth.js
+++ routes/auth.js
@@ -58,5 +58,11 @@ router.route('/login')
     });
 });
 
+router.get('/auth/facebook',passport.authenticate('facebook',{scope:'email'}));
+
+router.get('/auth/facebook/callback',passport.authenticate('facebook',{
+  successRedirect:'/',
+  failureRedirect:'/'
+}));
 
   module.exports = router;
